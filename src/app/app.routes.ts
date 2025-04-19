0bcfe2414d0a80e2291ba24689eb74b8963558ac
AssumedDialogue <114254777+AssumedDialogue@users.noreply.github.com>
2025-04-18 23:34:08 -0400

    added the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { CreateBookComponent } from './components/create-book/create-book.component';
+import { UpdateBookComponent } from './components/update-book/update-book.component';
+import { FindBookComponent } from './components/find-book/find-book.component';
+import { DeleteBookComponent } from './components/delete-book/delete-book.component';
+import { BookListComponent } from './components/book-list/book-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  { path: 'books', component: BookListComponent },
+  { path: 'books/add', component: CreateBookComponent },
+  { path: 'books/:id', component: FindBookComponent },
+  { path: 'books/:id/update', component: UpdateBookComponent },
+  { path: 'books/:id/delete', component: DeleteBookComponent }
+];
